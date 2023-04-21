f7828bcb895c0e7425280150cfbecdc969227972
Mike <mskowron4@yahoo.com>
2023-04-20 19:48:35 -0700

    added javascript page

---
+++ script.js
@@ -0,0 +1,8 @@
+var emailBtn = document.querySelector("#email");
+
+function showEmail() {
+    console.log('test');
+    alert('mskowron4@yahoo.com');
+}
+
+emailBtn.addEventListener("click", showEmail);
